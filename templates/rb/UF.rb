28e90960a5e4456c149369b302cde6f2f9300778
liuchia <lauch@tcd.ie>
2019-10-10 20:50:14 +0000

    Improved UF

--- templates/rb/UF.rb
+++ templates/rb/UF.rb
@@ -1,26 +1,20 @@
-class UF
-	attr_accessor :parent, :height
-	def initialize(n)
-		@parent = Array.new(n) {|i| i}
-		@height = Array.new(n, 0)
-	end
-	def find(i)
-		return i if @parent[i] == i
-		@parent[i] = find(@parent[i])
-		return @parent[i]
-	end
-	def connected(i, j)
-		return find(i) == find(j)
-	end
-	def union(i, j)
-		if !connected(i, j)
-			x, y = find(i), find(j)
-			if @height[x] > @height[y]
-				@parent[y] = x
-			else
-				@parent[x] = y
-				@height[y] += 1 if @height[x] == @height[y]
-			end
-		end
+# Needed: n
+Parent = Array.new(n) {|i| i}
+Height = Array.new(n, 0)
+
+def find(a)
+	return a if a == Parent[a]
+	Parent[a] = find(Parent[a])
+end
+
+def union(a, b)
+	ra = find(a)
+	rb = find(b)
+	if Height[ra] > Height[rb]
+		Parent[rb] = ra
+	else
+		Parent[ra] = rb
+		Height[rb] += 1 if Height[ra] == Height[rb]
 	end
 end
+
